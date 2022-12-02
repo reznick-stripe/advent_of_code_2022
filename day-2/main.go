3e291a6cabc1c42877da59703fc9ad7382df7141
Tom Reznick <reznick@stripe.com>
2022-12-02 15:14:49 -0500

    debugging

--- day-2/main.go
+++ day-2/main.go
@@ -13,6 +13,17 @@ type Choice struct {
 	Ties  *Choice
 	Loses *Choice
 	Score int
+	Name  string
+}
+
+var logIO = bufio.NewWriter(os.Stdout)
+
+func logIt(s string) {
+	fmt.Fprint(logIO, fmt.Sprintf("%s ", s))
+}
+
+func debug() bool {
+	return os.Getenv("DEBUG") == "true"
 }
 
 func (c *Choice) WinsAgainst(o *Choice) bool {
@@ -28,20 +39,29 @@ func (c *Choice) LosesAgainst(o *Choice) bool {
 }
 
 var youWin = func(opponentChoice *Choice) *Choice {
+	if debug() {
+		logIt("desired_outcome=win")
+	}
 	return opponentChoice.Loses
 }
 
 var youLose = func(opponentChoice *Choice) *Choice {
+	if debug() {
+		logIt("desired_outcome=lose")
+	}
 	return opponentChoice.Beats
 }
 
 var youTie = func(opponentChoice *Choice) *Choice {
+	if debug() {
+		logIt("desired_outcome=tie")
+	}
 	return opponentChoice.Ties
 }
 
-var rock = Choice{Score: 1}
-var paper = Choice{Score: 2}
-var scissors = Choice{Score: 3}
+var rock = Choice{Name: "rock", Score: 1}
+var paper = Choice{Name: "paper", Score: 2}
+var scissors = Choice{Name: "scissors", Score: 3}
 
 var opponentChoiceMap = map[string]*Choice{
 	"A": &rock,
@@ -56,15 +76,28 @@ var yourChoiceMap = map[string](func(*Choice) *Choice){
 }
 
 func ScoreRound(opponentChoice *Choice, yourChoice *Choice) int {
+	if debug() {
+		logIt(fmt.Sprintf("opponent_choice=%s your_choice=%s", opponentChoice.Name, yourChoice.Name))
+		logIt(fmt.Sprintf("your_choice_score=%d", yourChoice.Score))
+	}
 
 	if yourChoice.WinsAgainst(opponentChoice) {
+		if debug() {
+			logIt("outcome=win outcome_score=6")
+		}
 		return yourChoice.Score + 6
 	}
 
 	if yourChoice.TiesAgainst(opponentChoice) {
+		if debug() {
+			logIt("outcome=tie outcome_score=3")
+		}
 		return yourChoice.Score + 3
 	}
 
+	if debug() {
+		logIt("outcome=loss outcome_score=0")
+	}
 	return yourChoice.Score
 }
 
@@ -99,9 +132,17 @@ func main() {
 	for scanner.Scan() {
 		str := scanner.Text()
 		opponentInput, outcomeInput := unpack(str)
+		if debug() {
+			logIt(fmt.Sprintf("opponent_input=%s outcome_input=%s", opponentInput, outcomeInput))
+		}
 		opponentChoice := opponentChoiceMap[opponentInput]
 		yourChoice := yourChoiceMap[outcomeInput](opponentChoice)
 		score += ScoreRound(opponentChoice, yourChoice)
+		if debug() {
+			logIt(fmt.Sprintf("total_score=%d", score))
+			logIt("\n")
+			logIO.Flush()
+		}
 	}
 
 	fmt.Printf("highest score: %d\n", score)
