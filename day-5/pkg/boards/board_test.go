69399fa24411531b72ed9adcb83ecff608bd68ea
Tom Reznick <reznick@stripe.com>
2022-12-05 21:42:48 -0500

    e2e test

--- day-5/pkg/boards/board_test.go
+++ day-5/pkg/boards/board_test.go
@@ -1,6 +1,7 @@
 package boards
 
 import (
+	"bufio"
 	. "main/pkg/crates"
 	. "main/pkg/instructions"
 	"strings"
@@ -30,6 +31,29 @@ func TestBoardMove(t *testing.T) {
 			if b[1][i] != r {
 				t.Errorf("expected the new crate to have %c in index %d, but it had %c", r, i, b[1][i])
 			}
+		}
+	})
+
+	t.Run("end 2 end", func(t *testing.T) {
+		b := Board{Crate{'A', 'B', 'C'}, Crate{'D', 'E', 'F'}, Crate{'G', 'H', 'I'}}
+		input := ` move 1 from 0 to 1
+ move 1 from 2 to 0
+ move 2 from 1 to 2`
+
+		scanner := bufio.NewScanner(strings.NewReader(input))
+
+		for scanner.Scan() {
+			inst, err := NewInstructionFromInput(scanner.Text())
+			if err != nil {
+				t.Errorf("expected no error but got %s", err)
+			}
+			err = b.Move(inst)
+		}
+
+		output := b.Top()
+
+		if output != "IEF" {
+			t.Errorf("expected 'ICH' but got %s", output)
 		}
 	})
 
