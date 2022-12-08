6d5442be49d9ab60d36e9ef1e2b79194be0180ae
Tom Reznick <reznick@stripe.com>
2022-12-08 11:45:09 -0500

    count treemaps

--- day-8/pkg/treemap/treemap.go
+++ day-8/pkg/treemap/treemap.go
@@ -1,3 +1,66 @@
 package treemap
 
 type TreeMap [][]int
+
+func (t TreeMap) CountForARow(row int, vantage string) int {
+	visibleCount := 0
+	biggestYet := 0
+	r := t[row]
+	if vantage == "east" {
+		// facing east from the west
+		for i, n := range r {
+			if i == 0 || n > biggestYet {
+				visibleCount++
+				biggestYet = n
+				continue
+			}
+		}
+	} else if vantage == "west" {
+		// facing west from the east
+		endIndex := len(r) - 1
+		for i := endIndex; i >= 0; i-- {
+			n := r[i]
+			if i == endIndex || n > biggestYet {
+				visibleCount++
+				biggestYet = n
+				continue
+			}
+		}
+	} else {
+		panic("invalid direction")
+	}
+
+	return visibleCount
+}
+
+func (t TreeMap) CountForAColumn(col int, vantage string) int {
+	visibleCount := 0
+	biggestYet := 0
+
+	if vantage == "south" {
+		// facing south from the north
+		for i, row := range t {
+			n := row[col]
+			if i == 0 || n > biggestYet {
+				visibleCount++
+				biggestYet = n
+				continue
+			}
+		}
+	} else if vantage == "north" {
+		// facing north from the south
+		endIndex := len(t) - 1
+		for i := endIndex; i >= 0; i-- {
+			n := t[i][col]
+			if i == endIndex || n > biggestYet {
+				visibleCount++
+				biggestYet = n
+				continue
+			}
+		}
+	} else {
+		panic("invalid direction")
+	}
+
+	return visibleCount
+}
---
+++ day-8/pkg/treemap/treemap_test.go
@@ -0,0 +1,75 @@
+package treemap
+
+import "testing"
+
+func TestCountForARow(t *testing.T) {
+	t.Run("east", func(t *testing.T) {
+		m := TreeMap{
+			{3, 0, 3, 7, 3},
+			{2, 5, 5, 1, 2},
+			{6, 5, 3, 3, 2},
+			{3, 3, 5, 4, 9},
+			{3, 5, 3, 9, 0},
+		}
+
+		row := 1
+		expected := 2
+		actual := m.CountForARow(row, "east")
+		if expected != actual {
+			t.Errorf("expected %d for row %d but got %d instead", expected, row, actual)
+		}
+	})
+
+	t.Run("west", func(t *testing.T) {
+		m := TreeMap{
+			{3, 0, 3, 7, 3},
+			{2, 5, 5, 1, 2},
+			{6, 5, 3, 3, 2},
+			{3, 3, 5, 4, 9},
+			{3, 5, 3, 9, 0},
+		}
+
+		row := 4
+		expected := 2
+		actual := m.CountForARow(row, "west")
+		if expected != actual {
+			t.Errorf("expected %d for row %d but got %d instead", expected, row, actual)
+		}
+	})
+}
+
+func TestCountForAColumn(t *testing.T) {
+	t.Run("north", func(t *testing.T) {
+		m := TreeMap{
+			{3, 0, 3, 7, 3},
+			{2, 5, 5, 1, 2},
+			{6, 5, 3, 3, 2},
+			{3, 3, 5, 4, 9},
+			{3, 5, 3, 9, 0},
+		}
+
+		col := 2
+		expected := 2
+		actual := m.CountForAColumn(col, "north")
+		if expected != actual {
+			t.Errorf("expected %d for column %d but got %d instead", expected, col, actual)
+		}
+	})
+
+	t.Run("south", func(t *testing.T) {
+		m := TreeMap{
+			{3, 0, 3, 7, 3},
+			{2, 5, 5, 1, 2},
+			{6, 5, 3, 3, 2},
+			{3, 3, 5, 4, 9},
+			{3, 5, 3, 6, 0},
+		}
+
+		col := 3
+		expected := 1
+		actual := m.CountForAColumn(col, "south")
+		if expected != actual {
+			t.Errorf("expected %d for column %d but got %d instead", expected, col, actual)
+		}
+	})
+}
