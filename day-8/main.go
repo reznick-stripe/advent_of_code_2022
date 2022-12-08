f069015e8305e68983408fab6dcd96ba4c5e00a5
Tom Reznick <reznick@stripe.com>
2022-12-08 16:29:31 -0500

    day 8 part 1

---
+++ day-8/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	. "main/pkg/parser"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	treeMap, err := Parse(scanner)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sum := treeMap.Count()
+
+	fmt.Println(fmt.Sprintf("sum=%d", sum))
+}
