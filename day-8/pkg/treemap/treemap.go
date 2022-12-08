8fa836ca2c4356ffcb9cfb9add2aedca5e3b23b8
Tom Reznick <reznick@stripe.com>
2022-12-08 17:46:45 -0500

    cyclic import

--- day-8/pkg/treemap/treemap.go
+++ day-8/pkg/treemap/treemap.go
@@ -4,6 +4,77 @@ import (
 	"fmt"
 )
 
+type Direction int
+
+const (
+	North Direction = iota
+	South
+	East
+	West
+)
+
+func (d Direction) String() string {
+	return [...]string{"north", "south", "east", "west"}[d]
+}
+
+func GetScoreForDirection(tree *TreeMap, row int, col int, direction Direction) int {
+	count := 0
+	startingHeight := tree.Data[row][col]
+
+	switch direction {
+	case North:
+		for i := row - 1; i >= 0; i-- {
+			if tree.Data[i][col] < startingHeight {
+				count++
+			} else {
+				count++
+				break
+			}
+		}
+	case South:
+		for i := row + 1; i < tree.RowCount; i++ {
+			if tree.Data[i][col] < startingHeight {
+				count++
+			} else {
+				count++
+				break
+			}
+		}
+	case East:
+		for i := col + 1; i < tree.ColCount; i++ {
+			if tree.Data[row][i] < startingHeight {
+				count++
+			} else {
+				count++
+				break
+			}
+		}
+	case West:
+		for i := col - 1; i >= 0; i-- {
+			if tree.Data[row][i] < startingHeight {
+				count++
+			} else {
+				count++
+				break
+			}
+		}
+	}
+
+	return count
+}
+
+func GetScoreForLocation(tree *TreeMap, row int, col int) int {
+	compass := []Direction{North, South, East, West}
+
+	product := 1
+
+	for _, d := range compass {
+		product *= GetScoreForDirection(tree, row, col, d)
+	}
+
+	return product
+}
+
 type TreeMap struct {
 	Data       [][]int
 	RowCount   int
--- day-8/pkg/treemap/treemap_test.go
+++ day-8/pkg/treemap/treemap_test.go
@@ -35,3 +35,75 @@ func TestCount(t *testing.T) {
 		t.Errorf("expected %d but got %d", expected, output)
 	}
 }
+
+func TestGetScoreForDirection(t *testing.T) {
+	m := TreeMap{
+		Data: [][]int{
+			{3, 0, 3, 7, 3},
+			{2, 5, 5, 1, 2},
+			{6, 5, 3, 3, 2},
+			{3, 3, 5, 4, 9},
+			{3, 5, 3, 9, 0},
+		},
+		RowCount: 5,
+		ColCount: 5,
+		VisibleMap: [][]rune{
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+		},
+	}
+
+	row := 3
+	col := 2
+
+	testCases := []struct {
+		Expected int
+		Actual   int
+		Dir      Direction
+	}{
+		{2, GetScoreForDirection(&m, row, col, North), North},
+		{1, GetScoreForDirection(&m, row, col, South), South},
+		{2, GetScoreForDirection(&m, row, col, East), East},
+		{2, GetScoreForDirection(&m, row, col, West), West},
+	}
+
+	for _, c := range testCases {
+		if c.Expected != c.Actual {
+			t.Errorf("expected %d but got %d for (%d,%d) and %s", c.Expected, c.Actual, row, col, c.Dir)
+		}
+	}
+}
+
+func TestGetScoreForLocation(t *testing.T) {
+	m := TreeMap{
+		Data: [][]int{
+			{3, 0, 3, 7, 3},
+			{2, 5, 5, 1, 2},
+			{6, 5, 3, 3, 2},
+			{3, 3, 5, 4, 9},
+			{3, 5, 3, 9, 0},
+		},
+		RowCount: 5,
+		ColCount: 5,
+		VisibleMap: [][]rune{
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+			{'n', 'n', 'n', 'n', 'n'},
+		},
+	}
+
+	row := 3
+	col := 2
+
+	expected := 8
+	actual := GetScoreForLocation(&m, row, col)
+
+	if actual != expected {
+		t.Errorf("expected %d but got %d for (%d,%d)", expected, actual, row, col)
+	}
+}
